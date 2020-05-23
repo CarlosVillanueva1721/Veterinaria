86dd942c1928353f4ccd79f38faf3561a7bc719a
CarlosVillanueva1721 <60408267+CarlosVillanueva1721@users.noreply.github.com>
2020-05-23 14:37:14 -0700

    proyecto inicila veterinaria

---
+++ Veterinaria/Veterianria.Common/Mascota.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Veterianria.Common
+{
+    class Mascota
+    {
+        public int Id { get; set; }
+        public String Nombre { get; set; }
+        public DateTime? FechaNacimientos { get; set; }
+    }
+}
